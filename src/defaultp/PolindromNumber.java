a60b79bcfaa0d67aaf5ce5b4c0e10b96faecebde
rajeshacu <rajeshacu.mca23@rvce.edu.in>
2025-06-02 13:21:19 +0530

    Day2

---
+++ src/defaultp/PolindromNumber.java
@@ -0,0 +1,27 @@
+package defaultp;
+
+public class PolindromNumber {
+
+	public static void main(String[] args) {
+		int x=101;
+		int rev=0;
+		int original=x;
+		if(x>0) {
+			while(x!=0) {
+				int rem=x%10;
+				rev=rev*10+rem;
+				x=x/10;
+				
+			}
+
+			if(rev==original) {
+
+				System.out.println("polindrom "+rev);
+			}
+			else {
+				System.out.println("not polindrom");
+			}
+		}
+	}
+
+}
