96b94bbe8764e39fef1a1ff7092f2f6019e6e67a
rajesha c u <160035794+rajeshacu@users.noreply.github.com>
2025-06-20 15:43:51 +0530

    Time: 23 ms (8.09%), Space: 57.4 MB (94.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -5,18 +5,16 @@ class Solution {
         for(int i:nums){
 
             map.put(i,map.getOrDefault(i,0)+1);
-        }
-            for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-                if(entry.getValue()>1){
-
-                    return true;
+            if (map.get(i)>1)
+            return true;
                     
+        }
+            
+
+                 return false;   
                 }
 
-            }
-
         
-        return false;
+        
         
     }
-}
